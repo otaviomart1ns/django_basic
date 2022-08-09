b34addf862c57c4698380cf7e43b6ea8cff19358
Otavio Martins <otavio.martins01@hotmail.com>
2022-08-09 11:00:19 -0300

    preparando projeto para integração com Heroku

--- django_basic/settings.py
+++ django_basic/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%lfyl%fv8nl=z1eb-47_$ke^a#-fi4c-9w2g)e&pbe)5-z_br&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
